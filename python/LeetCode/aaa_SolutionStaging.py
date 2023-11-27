fcca78f9ba161fff08a021c2fc06cdde736f4a53
i-vt <99633800+i-vt@users.noreply.github.com>
2023-11-27 03:20:23 -0500

    Create aaa_SolutionStaging.py

---
+++ python/LeetCode/aaa_SolutionStaging.py
@@ -0,0 +1,8 @@
+class Solution: # Don't forget to remove the initiator "self" variable
+    #def convert(s: str, numRows: int) -> str:
+
+
+if __name__ == "__main__":
+  
+    var = Solution.convert("abcSFSssfSdefg", 4)  #Pls don't forget to change the function name from solution
+    print(var)
